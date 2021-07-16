826100bf1166662d8eb7fb12e28a78cf15fb9e79
NatPopov <natpopov007@gmail.com>
2021-07-16 18:36:29 +0800

    usd AccountInfo: 'AccountInfoWithDualRefCount'

--- packages/type-definitions/src/spec/hammer.ts
+++ packages/type-definitions/src/spec/hammer.ts
@@ -1,9 +1,9 @@
 import type { OverrideVersionedType } from '@polkadot/types/types';
-
+//todo: AccountInfo hardcoded for Substrate v3.0.0 https://github.com/polkadot-js/api/issues/3641
 const versioned: OverrideVersionedType[] = [
   {
-    minmax: [105, undefined],
-    types: {}
+    minmax: [undefined, undefined],
+    types: {AccountInfo: 'AccountInfoWithDualRefCount'}
   }
 ];
 
